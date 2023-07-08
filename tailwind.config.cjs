ba52072b0244ce24f2364827cb6ad4c07f7ef8fe
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-07-08 11:59:56 -0500

    feat(chat): Keyboard / input component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,9 +9,11 @@ module.exports = {
         "light-violet": "hsl(264, 100%, 61%)",
         "light-grayish-violet": "hsl(270, 20%, 96%)",
         "pale-violet": "hsl(276, 100%, 81%)",
+        "grayish-blue": "hsl(206, 6%, 79%)",
         // Chats
         "moderate-violet": "hsl(276, 55%, 52%)",
         "desaturated-violet": "hsl(271, 15%, 43%)",
+        "very-dark-desaturated-violet": "hsl(271, 36%, 24%)",
       },
     },
   },
