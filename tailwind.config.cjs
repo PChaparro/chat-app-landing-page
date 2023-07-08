7fc2967f100df952c6f988a0f69812a6d3cb0948
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-07-08 11:31:16 -0500

    feat(chat): `Header` and `Chat` components
    Show the text-chats and the header of the app

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "light-magenta": "hsl(293, 100%, 63%)",
+        "pale-magenta": "hsl(272, 46%, 93%)",
+        "light-violet": "hsl(264, 100%, 61%)",
+        "light-grayish-violet": "hsl(270, 20%, 96%)",
+        "pale-violet": "hsl(276, 100%, 81%)",
+        // Chats
+        "moderate-violet": "hsl(276, 55%, 52%)",
+        "desaturated-violet": "hsl(271, 15%, 43%)",
+      },
+    },
+  },
+  plugins: [],
+};
